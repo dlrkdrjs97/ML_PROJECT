b5affea48c81a837e1a500cddd69b20cd123452e
dlrkdrjs97 <dlrkdrjs97@gmail.com>
2018-06-11 20:47:15 +0900

    dd

--- Top_modeule.py
+++ Top_modeule.py
@@ -14,6 +14,7 @@ df = pd.read_csv("application_train.csv")
 df_test = pd.read_csv("application_test.csv")
 
 ## MODULE # 1
+print("MODULE # 1")
 m1 = MODULE1.XGB()
 m1.read_data(df)
 ret_val = m1.entire_training_process()
@@ -21,7 +22,8 @@ acc_train, acc_test = m1.figure_out_training_data()
 print("Train AUC %.4f"%acc_train)
 print("Test AUC %.4f"%acc_test)
 
-## MODULE # 1
+## MODULE # 2
+print("MODULE # 2")
 m2 = MODULE2.XGB()
 m2.read_data(df)
 ret_val = m2.entire_training_process()
@@ -29,7 +31,8 @@ acc_train, acc_test = m3.figure_out_training_data()
 print("Train AUC %.4f"%acc_train)
 print("Test AUC %.4f"%acc_test)
 
-## MODULE # 1
+## MODULE # 3
+print("MODULE # 3")
 m3 = MODULE3.XGB()
 m3.read_data(df)
 ret_val = m3.entire_training_process()
@@ -37,7 +40,8 @@ acc_train, acc_test = m3.figure_out_training_data()
 print("Train AUC %.4f"%acc_train)
 print("Test AUC %.4f"%acc_test)
 
-## MODULE # 1
+## MODULE # 4
+print("MODULE # 4")
 m4 = MODULE1.XGB()
 m4.read_data(df)
 ret_val = m4.entire_training_process()
@@ -45,7 +49,8 @@ acc_train, acc_test = m4.figure_out_training_data()
 print("Train AUC %.4f"%acc_train)
 print("Test AUC %.4f"%acc_test)
 
-## MODULE # 1
+## MODULE # 5
+print("MODULE # 5")
 m5 = MODULE1.XGB()
 m5.read_data(df)
 ret_val = m5.entire_training_process()
