702962f14d9cac5c92fcb2f2c2b6eac215a2d995
dlrkdrjs97 <dlrkdrjs97@gmail.com>
2018-06-13 13:07:20 +0900

    dd

--- Main_model.py
+++ Main_model.py
@@ -1,7 +1,8 @@
 ## Import extern module
 import pandas as pd
-import prediction_module as pm
+import Prediction_module as pm
 
+'''
 ## Read data set
 train_data = pd.read_csv("application_train.csv")
 test_data = pd.read_csv("application_test.csv")
@@ -9,3 +10,4 @@ test_data = pd.read_csv("application_test.csv")
 ## Training Session
 tp = pm.trainning_prediction(train_data, test_data)
 tp.entire_training_process()
+'''
