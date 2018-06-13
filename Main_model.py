8157db053de9d5f45dc393a930dcf267f1409ad2
dlrkdrjs97 <dlrkdrjs97@gmail.com>
2018-06-13 13:08:04 +0900

    dd

--- Main_model.py
+++ Main_model.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import Prediction_module as pm
 
-'''
+
 ## Read data set
 train_data = pd.read_csv("application_train.csv")
 test_data = pd.read_csv("application_test.csv")
@@ -10,4 +10,3 @@ test_data = pd.read_csv("application_test.csv")
 ## Training Session
 tp = pm.trainning_prediction(train_data, test_data)
 tp.entire_training_process()
-'''
